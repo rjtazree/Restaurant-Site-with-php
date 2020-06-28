cae061e649941bd1be00c840f174580d0eb3a6ed
rjtazree@gmail.com <rjtazree@gmail.com>
2020-06-28 21:39:04 +0600

    commit

--- index.php
+++ index.php
@@ -36,6 +36,40 @@
         <div id="nav">
 
         </div>
+
+
+        <div id="content">
+            
+            <div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Phone : </strong>
+                    112345678
+                </div><!--column--->
+
+                <div class="column three">
+                    <strong>Location : </strong>
+                    16 /D Chittagong.
+                </div>
+
+                <div class="column three last">
+                    <strong>Hours : </strong>
+                    <em>Tuesday -  Thursday</em><br>
+                    1:00m - 9:00pm<br><br>
+
+                    <em>Friday -  Saturday</em><br>
+                    4:00m - 11:00pm<br><br>
+
+                    <em>Sunday -  Monday</em><br>
+                    Closed<br><br>
+
+
+                </div>
+
+
+
+            </div>
+
+        </div>
         
     </div>
 
